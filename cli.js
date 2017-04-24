2d18cc96694dbc586b241ddb3a877906012c5bfb
Tim Scanlin <tscanlin@gmail.com>
2017-04-23 17:22:46 -0700

    add start command

--- cli.js
+++ cli.js
@@ -10,20 +10,10 @@ if (process.argv && process.argv.length > 2) {
   defaultOptions.outputFile = '' // Default to no output file over cli because of stdout.
   const options = Object.assign({}, defaultOptions, argv)
 
-  // options._.forEach((arg, i) => {
-  //   if (arg.indexOf('.html') === arg.length - 5) {
-  //     options.html.push(arg)
-  //   } else if (arg.indexOf('.css') === arg.length - 4) {
-  //     options.css.push(arg)
-  //   }
-  //   // TODO: Set more CLI options here.
-  // })
-
   processto(options, (err, data) => {
-    if (options.stdout) {
-      process.stdout.write(data.css)
-    }
-    // process.exit(0)
+    // if (options.stdout) {
+    //   process.stdout.write(data.css)
+    // }
   })
 } else {
   throw new Error('You need to pass arguments to css-razor')
